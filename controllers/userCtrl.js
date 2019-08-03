f2146a93cff3508e14612ba2703da62ce54733b7
bhardwajat <bhardwajakshat92@gmail.com>
2019-08-03 08:58:07 -0400

    Backend

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
 
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
@@ -10,8 +11,8 @@ var mysqlConnection = mysql.createConnection({
 
 exports.createUser = (req, res, next) => {
     let user = req.body;
-    var sql = "INSERT INTO user(email, password) VALUES (?, ?);"
-    mysqlConnection.query(sql, [user.email, user.password], (err, rows, fields)=>{
+    var sql = "INSERT INTO usercredentials(username, password) VALUES (?, ?);"
+    mysqlConnection.query(sql, [user.username, user.password], (err, rows, fields)=>{
         if(!err)
             res.send('Insert successful!');
         else
@@ -22,13 +23,32 @@ exports.createUser = (req, res, next) => {
 
 exports.loginUser = (req, res) => {
     let user = req.body;
-    var sql = "SELECT id FROM user WHERE email = ? AND password = ?;"
-    mysqlConnection.query(sql, [user.email, user.password], function(err, rows){
+    var sql = "SELECT * FROM usercredentials WHERE username = ? AND password = ?;"
+    mysqlConnection.query(sql, [user.username, user.password], function(err, rows){
         if(!err){
-            res.json(rows[0].id);
+            const user = {
+                id: rows[0].id,
+                username: rows[0].username,
+                password: rows[0].password
+            }
+            let token = jwt.sign({user: user}, 'jk23!+!97');
+            console.log(user);
+            res.status(200).send({token});
         }
         else{
             console.log(err);
         }
     })
+}
+
+exports.addEvent = (req, res, next) => {
+    let event = req.body;
+    var sql = "INSERT INTO appointment(userId, title, description, startTime, endTime, location, isMedication) VALUES(?, ?, ?, ?, ?, ?, ?);"
+    mysqlConnection.query(sql, [event.userId, event.title, event.description, event.startTime, event.endTime, event.location, event.isMedication], (err, rows, fields)=>{
+        if(!err)
+            res.send('Event inserted succesfully');
+        else
+            console.log(err);
+    })
+
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const UserCtrl = require('../controllers/userCtrl');
 
 router.post('/signup/user', UserCtrl.createUser);
 router.post('/login/user', UserCtrl.loginUser);
+router.post('/calendar', UserCtrl.addEvent);
 
 module.exports = router;

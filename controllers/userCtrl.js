d43c21da60bf6bf6e36c9286ee5bbced86a89f2a
bhardwajat <bhardwajakshat92@gmail.com>
2019-09-02 22:42:12 -0400

    Local Storage

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -22,23 +22,29 @@ exports.createUser = (req, res, next) => {
 
 
 exports.loginUser = (req, res) => {
+    console.log("~~~~  START of exports.loginUser")
+
     let user = req.body;
     var sql = "SELECT * FROM usercredentials WHERE username = ? AND password = ?;"
     mysqlConnection.query(sql, [user.username, user.password], function(err, rows){
-        if(!err){
+        if(err){
+            console.log("000Inside loginUser - got error while getting user data frm DB. Error:" + err);          
+        }
+        else{
+            console.log("111Inside exports.loginUser - going to get user credentional frm DB & than going to generate token");
             const user = {
                 id: rows[0].id,
                 username: rows[0].username,
                 password: rows[0].password
             }
-            let token = jwt.sign({user: user}, 'jk23!+!97');
-            console.log(user);
+            // to generae the new token & send it
+            let token = jwt.sign({user: user}, 'jk23!+!97', {expiresIn: '1min'});
+            console.log("222inside exports.loginUser Token created & going to send. Token:" + token);
             res.status(200).send({token});
         }
-        else{
-            console.log(err);
-        }
     })
+
+    console.log("~~~~  END of exports.loginUser")
 }
 
 exports.addEvent = (req, res, next) => {
